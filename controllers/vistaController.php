c170b06b3517065f06f3407b84d4d54ff6427375
Sam <programainline@gmail.com>
2021-12-07 12:04:42 -0600

    Agregadas vistas y en modelo lista blanca

--- controllers/vistaController.php
+++ controllers/vistaController.php
@@ -5,14 +5,12 @@ class vistaController extends vistaModel{
         return require_once "./view/plantilla.php";
     }
     public function getVistasController(){
-        error_log("entrando a getVistasController");
         if(isset($_GET['views'])){
             $ruta = explode("/",$_GET['views']);
             $respuesta = vistaModel::getVistasModel($ruta[0]);
         }else{
             $respuesta = 'login';
         }
-        error_log($respuesta);
         return $respuesta;
     }
 }
--- models/vistaModel.php
+++ models/vistaModel.php
@@ -2,7 +2,12 @@
 class vistaModel{
     protected static function getVistasModel($vistas){
         //list white of words in views, only access of url;
-        $listaBlanca = ["home","cliente-list"];
+        $listaBlanca = ["home","cliente-list","cliente-new","reservation-new",
+                        "client-search","client-update","company","item-list",
+                        "item-new","item-search","item-update","reservation-list",
+                         "reservation-new","reservation-pending","reservation-reservation",
+                         "reservation-search","reservation-update","user-list",
+                        "user-new","user-search","user-update"];
         if(in_array($vistas,$listaBlanca)){
             if(is_file("./view/contenidos/".$vistas."-view.php")){
                 $contenido = "./view/contenidos/".$vistas."-view.php";
