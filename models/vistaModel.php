d5d20d47a965b4aa28319aa2c7a9c45de735ebd3
Sam <programainline@gmail.com>
2021-12-07 12:42:38 -0600

    fix enlaces

--- models/vistaModel.php
+++ models/vistaModel.php
@@ -2,11 +2,11 @@
 class vistaModel{
     protected static function getVistasModel($vistas){
         //list white of words in views, only access of url;
-        $listaBlanca = ["home","cliente-list","cliente-new","reservation-new",
+        $listaBlanca = ["home","client-list","client-new","reservation-new",
                         "client-search","client-update","company","item-list",
                         "item-new","item-search","item-update","reservation-list",
-                         "reservation-new","reservation-pending","reservation-reservation",
-                         "reservation-search","reservation-update","user-list",
+                        "reservation-new","reservation-pending","reservation-reservation",
+                        "reservation-search","reservation-update","user-list",
                         "user-new","user-search","user-update"];
         if(in_array($vistas,$listaBlanca)){
             if(is_file("./view/contenidos/".$vistas."-view.php")){
